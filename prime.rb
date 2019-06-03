e18c0e6a735257010db88133876ec398754db63f
Francis Mendez <fmendez884@gmail.com>
2019-06-03 22:55:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
-# Add  code here!
-# def prime?(n)
-#   (2..n/2).none?{|i| n % i == 0}
-# end 
+require 'pry'
+
 
 def prime?(n)
   for i in 2..n-1
+    binding.pry
     if n % i == 0
       return false 
     else 
