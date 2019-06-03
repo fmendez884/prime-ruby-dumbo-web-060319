a7620f8ea5f7534e66217608be23315077366aa8
Francis Mendez <fmendez884@gmail.com>
2019-06-03 23:06:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,12 @@
 require 'pry'
 
 
-def prime?(n)
-  (2..n/2).none? do |i|
-    n % i == 0 || -n % -i == 0
-  end
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
 end
